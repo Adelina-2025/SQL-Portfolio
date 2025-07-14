76aae7fa81e31245e90dd2fd1e9d21f0a8703448
Adelina-2025 <choyu7979@gmail.com>
2025-07-14 13:08:01 -0700

    Delete LeetCode_SQL/001_FindPersons.sql

--- LeetCode_SQL/001_FindPersons.sql
+++
@@ -1,6 +0,0 @@
--- Problem: Report first name, last name, city, state for each person
--- If address missing, city and state should be NULL.
-
-SELECT firstName, lastName, city, state
-FROM Person
-LEFT JOIN Address ON Person.personId = Address.personId;
